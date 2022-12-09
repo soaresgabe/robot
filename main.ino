bea5ab2086cc1706379736df4e74d683d4fda9de
Gabe <gabesoares@proton.me>
2022-12-09 01:51:03 -0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,153 @@
+/*********
+  Gabriel Soares
+  Robô Wireless Multidirecional
+  Project: github.com/soaresgabe/robozinho-v1  
+*********/
+
+// Load Wi-Fi library
+#include <ESP8266WiFi.h>
+
+// Set Wi-Fi
+const char* ssid = "Robo | Gabe";
+const char* password = "999999999";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+String motorState = "off";
+
+// Assign output variables to GPIO pins
+#define IN1 D5
+#define IN2 D6
+#define IN3 D7
+#define IN4 D8
+
+void setup() {
+
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Set outputs to LOW
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();  // Listen for incoming clients
+
+  if (client) {                     // If a new client connects,
+    Serial.println("New Client.");  // print a message out in the serial port
+    String currentLine = "";        // make a String to hold incoming data from the client
+    while (client.connected()) {    // loop while the client's connected
+      if (client.available()) {     // if there's bytes to read from the client,
+        char c = client.read();     // read a byte, then
+        Serial.write(c);            // print it out the serial monitor
+        header += c;
+        if (c == '\n') {  // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // turns the motors on and off
+            if (header.indexOf("GET /motor/foward") >= 0 && motorState == "on") {
+              digitalWrite(IN1, HIGH);
+              digitalWrite(IN2, LOW);
+              digitalWrite(IN3, HIGH);
+              digitalWrite(IN4, LOW);
+            } else if (header.indexOf("GET /motor/backward") >= 0 && motorState == "on") {
+              digitalWrite(IN1, LOW);
+              digitalWrite(IN2, HIGH);
+              digitalWrite(IN3, LOW);
+              digitalWrite(IN4, HIGH);
+            } else if (header.indexOf("GET /motor/left") >= 0 && motorState == "on") {
+              digitalWrite(IN1, LOW);
+              digitalWrite(IN2, LOW);
+              digitalWrite(IN3, HIGH);
+              digitalWrite(IN4, LOW);
+            } else if (header.indexOf("GET /motor/right") >= 0 && motorState == "on") {
+              digitalWrite(IN1, HIGH);
+              digitalWrite(IN2, LOW);
+              digitalWrite(IN3, LOW);
+              digitalWrite(IN4, LOW);
+            } else if (header.indexOf("GET /motor/on") >= 0) {
+              motorState = "on";
+            } else if (header.indexOf("GET /motor/off") >= 0) {
+              motorState = "off";
+              digitalWrite(IN1, LOW);
+              digitalWrite(IN2, LOW);
+              digitalWrite(IN3, LOW);
+              digitalWrite(IN4, LOW);
+            }
+
+            // HTML HEAD
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+
+            // CSS
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: auto; text-align: center;}");
+            client.println(".button { background-color: #555555; color: white; padding: 16px 40px; border-radius: 50%;");
+            client.println("text-decoration: none; font-size: 30px; margin: 20px; cursor: pointer;}");
+            client.println(".on {background-color: #33ff33; border-radius: 0%}");
+            client.println(".off {background-color: #ff3333; border-radius: 0%}");
+            client.println(".div {  margin: auto;}");
+            client.println("</style></head>");
+
+            // HTML BODY
+            client.println("<body><h1>Robot | Web Server</h1>");
+            client.println("<div><a href=\"/motor/foward\"><button class=\"button\">^</button></a></div>");
+            client.println("<div><a href=\"/motor/left\"><button class=\"button\"><</button></a>");
+            client.println("<a href=\"/motor/right\"><button class=\"button\">></button></a></div>");
+            client.println("<div><a href=\"/motor/backward\"><button class=\"button\">v</button></a></div>");
+            client.println("<br><br>");
+            if (motorState == "off") {
+              client.println("<center><p><a href=\"/motor/on\"><button class=\"button off\">OFF</button></a></p></center>");
+            } else {
+              client.println("<center><p><a href=\"/motor/off\"><button class=\"button on\">ON</button></a></p></center>");
+            }
+            client.println("</body></html>");
+            client.println();
+
+            // Break out of the while loop
+            break;
+          } else {  // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
